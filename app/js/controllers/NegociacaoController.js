14247b19e7321f6a566c20637a8d52202ce19210
echinen <eric_chinen@hotmail.com>
2017-07-10 02:07:10 -0300

    add all project typescript example

---
+++ app/js/controllers/NegociacaoController.js
@@ -0,0 +1,19 @@
+class NegociacaoController {
+    constructor() {
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
+        this._mensagemView = new MensagemView('#mensagemView');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
+}
---
+++ app/ts/app.ts
@@ -0,0 +1,6 @@
+const controller = new NegociacaoController();
+
+$('.form').submit(controller.adiciona.bind(controller));
+/*document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));*/
